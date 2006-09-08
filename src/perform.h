9f09c34cc140b01c3f7ec46b01783bae6c6626aa
Mike Buland <mike@xagasoft.com>
2006-09-08 22:43:30 +0000

    Added targets to the performs.

--- src/perform.h
+++ src/perform.h
@@ -16,10 +16,18 @@ public:
 	void addParam( const char *sParam );
 	virtual Perform *duplicate( Build &bld, const std::string &cont ) = 0;
 	void copyData( Perform *pSrc, Build &bld, const std::string &cont );
+	std::string getTarget()
+	{
+		return sTarget;
+	}
+	void setTarget( std::string sTarget )
+	{
+		this->sTarget = sTarget;
+	}
 
 private:
 	std::list<std::string> lParam;
-
+	std::string sTarget;
 };
 
 #endif
