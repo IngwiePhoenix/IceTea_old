8a6c171b3bc24ad0e7b407dd37491fe1ca5274e0
David Braden V <neonphog@xagasoft.com>
2006-12-26 19:59:58 +0000

    david - woops, forgot to add

---
+++ src/viewercolorpct.h
@@ -0,0 +1,49 @@
+#ifndef VIEWER_COLOR_PCT_H
+#define VIEWER_COLOR_PCT_H
+
+#include <stdint.h>
+#include <list>
+#include "viewer.h"
+
+class ViewerColorPct : public Viewer
+{
+public:
+	ViewerColorPct();
+	virtual ~ViewerColorPct();
+
+	virtual void beginCommand( Action::eAction nAct, const std::string &sTarget );
+	virtual void endCommand();
+
+	virtual void beginRequiresCheck( bool bCached, const std::string &sName );
+	virtual void endRequiresCheck();
+	virtual void beginPerform( Perform *pPerform );
+	virtual void endPerform();
+	virtual void beginPerforms( int nCount );
+
+	virtual void skipRequiresCheck( bool bCached, const std::string &sName );
+	virtual void skipPerform( Perform *pPerform );
+
+	virtual void beginAction( const std::string &sName, int nCommands );
+
+	virtual void endAction();
+
+	void printPerform(const char *sRule, const char *sTarget);
+	void printHead();
+	void indent();
+
+private:
+	int iTC, iCC;
+	
+	bool bRunClean;
+	typedef struct
+	{
+		int nLevel;
+		bool bCmdClean;
+		std::string sTarget;
+		int iTP, iCP;
+	} Cmd;
+	typedef std::list<Cmd> CmdStack;
+	CmdStack sCmd;
+};
+
+#endif
